81844cbc9a942c4818d664b7c7e7dfa063a0d01f
raymond <raymond@nusantarabetastudio.com>
2023-03-08 13:48:15 +0700

    feat(formatter/date): Add formatSeconds Method And Test

--- src/formatter/date.ts
+++ src/formatter/date.ts
@@ -1,6 +1,16 @@
 import { DateTime } from "luxon";
 /**
- * formatCurrency
+ * formatSeconds
+ * example:
+ *
+ * - `formatSeconds(135533159, 'y-LL-d') // 2012-12-12`<br>
+ * [Check Formatter Table]{@link https://github.com/moment/luxon/blob/master/docs/formatting.md#table-of-tokens}
+ */
+export const formatSeconds = (seconds: number, format: string) =>
+  DateTime.fromSeconds(seconds).toFormat(format);
+
+/**
+ * formatJsDate
  * example:
  *
  * - `formatJsDate(new Date(2012,11,12), 'y-LL-d') // 2012-12-12`<br>
@@ -8,6 +18,7 @@ import { DateTime } from "luxon";
  */
 export const formatJsDate = (date: Date, format: string) =>
   DateTime.fromJSDate(date).toFormat(format);
+
 /**
  * formatCurrency
  * example:
--- test/formatter-date.test.ts
+++ test/formatter-date.test.ts
@@ -1,28 +1,36 @@
 import { describe, expect, test } from "@jest/globals";
-import { formatJsDate, formatJsDateToEpoch } from "../src/formatter/date";
+import {
+  formatSeconds,
+  formatJsDate,
+  formatJsDateToEpoch,
+} from "../src/formatter/date";
 
 describe("Testing All Formatter Date Success", () => {
-  test("new Date(2012,11,12) to 2012-12-12", () => {
+  test("1355331599 to 2012-12-12", () => {
+    expect(formatSeconds(1355331599, "y-LL-d")).toBe("2012-12-12");
+  });
+
+  test("new Date(2012, 11, 12) to be epoch 2012-12-12 ", () => {
     expect(formatJsDate(new Date(2012, 11, 12), "y-LL-d")).toBe("2012-12-12");
   });
 
-  test("new Date(2012,11,12,23,59,59) to be epoch 1355331599 ", () => {
-    expect(formatJsDateToEpoch(new Date(2012, 11, 12, 23, 59, 59))).toBe(
-      formatJsDateToEpoch(new Date(2012, 11, 12, 23, 59, 59))
-    );
+  test("new Date(2012,11,12,23,59,59) to be epoch 1355245200 ", () => {
+    expect(formatJsDateToEpoch(new Date(2012, 11, 12))).toBe(1355270400);
   });
 });
 
-describe("Testing All Formatter Date Error", () => {
-  test("new Date(2012,11,12) to 2013-12-12", () => {
-    expect(formatJsDate(new Date(2012, 12, 12), "y-LL-d")).not.toBe(
+describe("Testing All Formatter Number Error", () => {
+  test("formatSeconds when result not valid", () => {
+    expect(formatSeconds(1355331599, "y-LL-d")).not.toBe("2012-12-13");
+  });
+
+  test("formatJsDate when result not valid", () => {
+    expect(formatJsDate(new Date(2012, 11, 13), "y-LL-d")).not.toBe(
       "2012-12-12"
     );
   });
 
-  test("new Date(2012,11,12,23,59,59) to be epoch 135533159 ", () => {
-    expect(formatJsDateToEpoch(new Date(2012, 11, 12, 23, 59, 1))).not.toBe(
-      1355331599
-    );
+  test("formatJsDateToEpoch when result not valid", () => {
+    expect(formatJsDateToEpoch(new Date(2012, 11, 12))).not.toBe(1355245201);
   });
 });
